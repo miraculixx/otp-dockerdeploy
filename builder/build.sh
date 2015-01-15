cf55185fa16800b3f041f3baae2ec0fffd5b4f75
Pablo Fredrikson <pablo@fredrikson.com.ar>
2015-01-14 21:55:57 -0300

    Improving build script
    Now supports multiple urls and env variables to otp.

--- builder/build.sh
+++ builder/build.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+# Getting the urls and environment variables for otp
+while getopts ":u:e:" opt
+   do
+      case $opt in
+         u ) urls=$OPTARG;;
+         e ) envs=$OPTARG;;
+      esac
+done
+
+for url in $urls; do
+   wget -q -P /var/otp/pdx/ $url
+done
 
 # Defining the env variables
 export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
-
 mkdir -p /var/otp/pdx && \
-
-for url in $@; do
-   wget -q -P /var/otp/pdx/ $url && \
-done
-
+  
 # Let's build the maps
 cd /var/otp && \
 wget -q -O /var/otp/otp.jar http://dev.opentripplanner.org/jars/otp-latest-master.jar && \
-java -Xmx8G -jar /var/otp/otp.jar --build /var/otp/pdx
+java -Xmx8G -jar /var/otp/otp.jar --build /var/otp/pdx $envs
 
 # Moving the generated graph to the shared dir
 mkdir -p /var/otp/graphs && \
