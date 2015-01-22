c4fcef595e9ffa568ba13b9545ba47d285b0754f
root <Patrick Senti>
2015-01-22 08:44:59 -0500

    draft build script

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 # Define commonly used JAVA_HOME variable
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
+rm -rf /var/otp/pdx/*
 mkdir -p /var/otp/pdx
 #wget -q -O /var/otp/pdx/zurich_switzerland.osm.pbf https://s3.amazonaws.com/metro-extracts.mapzen.com/zurich_switzerland.osm.pbf
 #wget -q -O /var/otp/pdx/gtfs_complete.zip http://gtfs.geops.ch/dl/gtfs_complete.zip
-wget -q -O /var/otp/pdx/fix.zip http://www.gtfs-data-exchange.com/agency/flixbus-gmbh/latest.zip
+#wget -q -O /var/otp/pdx/fix.zip http://www.gtfs-data-exchange.com/agency/flixbus-gmbh/latest.zip
+wget -q -O /var/otp/pdx/fix.zip https://www.dropbox.com/s/2rpmww0nrrq00kj/Simplex%202014%20%281%29.zip?dl=0
 
 # Let's build the maps
 cd /var/otp
